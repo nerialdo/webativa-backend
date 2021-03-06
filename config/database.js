cb32a4b0be8e93384265ae6bea3fafcc55e34065
Nerialdo Ferreira <nerialdomoreira@hotmail.com.br>
2021-03-06 15:24:18 -0300

    ativar ssl para postgres no database.js

--- config/database.js
+++ config/database.js
@@ -73,11 +73,14 @@ module.exports = {
   pg: {
     client: 'pg',
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
-      port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis'),
+      host: Env.get('DB_HOST'),
+      // host: Env.get('DB_HOST', 'localhost'),
+      port: Env.get('DB_PORT'),
+      user: Env.get('DB_USER'),
+      // user: Env.get('DB_USER', 'root'),
+      password: Env.get('DB_PASSWORD'),
+      database: Env.get('DB_DATABASE'),
+      // database: Env.get('DB_DATABASE', 'adonis'),
       ssl: true
     },
     debug: Env.get('DB_DEBUG', false)
